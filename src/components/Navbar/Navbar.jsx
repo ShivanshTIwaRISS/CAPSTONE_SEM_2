163948107c69cfea291c4e423a704c75b8a45131
ShivanshTIwaRISS <shivansh092322120@gmail.com>
2025-07-27 21:43:28 +0530

    DonE

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,6 @@ export default function Navbar() {
   const [showDropdown, setShowDropdown] = useState(false);
   const navigate = useNavigate();
   const inputRef = useRef(null);
-
   useEffect(() => {
     if (!searchTerm.trim()) {
       setSuggestions([]);
